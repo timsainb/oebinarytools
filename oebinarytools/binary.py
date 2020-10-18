e55b0199606a13e80267ea5a025fa100ee5bf399
timsainb <timsainb@gmail.com>
2020-10-18 12:40:37 -0700

    fixed dat files

---
+++ oebinarytools/binary.py
@@ -0,0 +1,14 @@
+class BinaryExperiment():
+    def __init__(
+        self,
+        experiment_path, 
+    ):
+        return
+
+
+
+
+
+def merge_binary_experiments():
+    # TODO 
+    raise NotImplementedError
