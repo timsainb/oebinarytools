b9c748c0da6dfb7373ca078a14e23accf3b5ebed
timsainb <timsainb@gmail.com>
2020-10-17 17:21:03 -0700

    added sample dataset

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='oebinarytools',
+    packages=find_packages(),
+    version='0.1.0',
+    description='OpenEphys tools for loading binary data with rpioperant behavioor',
+    author='Tim Sainburg',
+    license='MIT',
+)
